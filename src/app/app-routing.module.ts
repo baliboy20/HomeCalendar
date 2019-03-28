03ccaa338a5d633d4f59db014923e7243611b76c
baliboy20 <williampaulton@yahoo.co.uk>
2019-03-28 22:21:01 +0000

    Revert "initial commit"
    This reverts commit ac2b6a0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {OverviewComponent} from './overview/overview.component';
-import {OverviewModule} from './overview/overview.module';
-import {SimpleDataEntryComponent} from './simple-data-entry/simple-data-entry.component';
 
-const routes: Routes = [
-    {path: 'overview', component: OverviewComponent},
-    {path: 'data-entry', component: SimpleDataEntryComponent},
-    {path: '', redirectTo: 'dataEntry', pathMatch: 'full'},
-];
+const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), OverviewModule],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
